95431bd15e1425c57b5c503c4bf1c54a3da01059
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2024-09-11 20:08:01 -0300

    Feat: Adição das perguntas

---
+++ Funcoes_js/ex1/script.js
@@ -0,0 +1,2 @@
+/* Ex1: Crie uma função que lê um vetor de 5 números inteiros e mostre-os, considere 
+que o usuário não pode informar letras, palavras ou vazio. Alerte-o do erro e repita a leitura caso ocorra.*/
---
+++ Funcoes_js/ex5/script.js
@@ -0,0 +1,2 @@
+/*Ex5: Crie um algoritmo usando funções que leia até 50 números inteiros e 
+armazene-os numa lista de números, considere as premissas abaixo:*/
