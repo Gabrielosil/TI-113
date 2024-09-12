f5b93143f7ff6bf1f2a5d47ea11014d94ee47a42
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2024-09-11 21:18:27 -0300

    Feat: Conclusão do exercicio 3.

--- Funcoes_js/ex3/script.js
+++ Funcoes_js/ex3/script.js
@@ -1,2 +1,27 @@
 /*Ex3: Crie um algoritmo usando funções que faça uma leitura de 10 letras em um vetor, e após verificar 
 quantas vogais foram lidas e mostre somente consoantes no alerta.*/
+
+function lerLetras() {
+    let msg = "";
+    let letras = [];
+    let cont = 0;
+    
+    for (let i = 0; i < 10; i++) {
+        let val = prompt("Informe a letra");
+        letras[i] = val;
+    }
+    
+    for (let j = 0; j < 10; j++) {
+        let val = letras[j].toUpperCase();
+        if (!(val == "A" || val == "E" || val == "I" || val == "O" || val == "U")) {
+            cont++;
+            msg += val + "\n";
+        }
+    }
+    
+    alert(msg + "Consoantes: " + cont);
+}
+
+lerLetras();
+
+console.log(lerLetras)
